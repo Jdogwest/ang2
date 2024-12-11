c17c48c7e64eea7bce63b58d4d878402d29ad814
Kulikova Darya <145858240+Veneyli@users.noreply.github.com>
2024-12-11 11:20:21 +0300

    change styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,17 @@
-import { StoreService } from './services/store.service';
 import { Component, inject } from '@angular/core';
-import { RouterModule, RouterOutlet } from '@angular/router';
+import {
+  RouterModule,
+  RouterOutlet,
+  Router,
+  NavigationEnd,
+} from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 import { CommonModule } from '@angular/common';
 import { Subscription } from 'rxjs';
 import { SidebarInfoComponent } from './components/sidebar-info/sidebar-info.component';
 import { MessageService } from './services/message.service';
+import { DialogComponent } from './components/dialog/dialog.component';
 
 @Component({
   selector: 'app-root',
@@ -16,17 +22,24 @@ import { MessageService } from './services/message.service';
 export class AppComponent {
   messageService: MessageService = inject(MessageService);
   subscription!: Subscription;
+  routerSubscription!: Subscription;
+  clientFound = false;
+
+  constructor(private router: Router) {}
+
   ngOnInit() {
+    this.routerSubscription = this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe((event: NavigationEnd) => {
+        this.clientFound = event.urlAfterRedirects !== '/';
+      });
+
     this.subscription = this.messageService.message$.subscribe(
       (message: boolean) => {
         this.clientFound = message;
       }
     );
   }
-  ngOnDestroy() {
-    this.subscription.unsubscribe();
-  }
 
   title = 'MedApp';
-  clientFound = false;
 }
